a2cd42d2c8f49bff2aa205650feeb2181b7d45a4
Realcapz <144321466+Realcapz@users.noreply.github.com>
2024-09-18 16:07:34 +0200

    changed configfile

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+// vite.config.js
+export default {
   build: {
     rollupOptions: {
-     
-      external: [],
-     
-    },
-  },
-});
+      external: ['react-router-dom']
+    }
+  }
+}
 
